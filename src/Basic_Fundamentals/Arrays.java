b880f3e6fecd1543879ae49207bd4a6b63f2db4f
Lei Justine <clemente.leijustine@gmail.com>
2025-09-23 18:24:17 +0800

    Add Arrays

---
+++ src/Basic_Fundamentals/Arrays.java
@@ -0,0 +1,11 @@
+package Basic_Fundamentals;
+
+public class Arrays {
+     public static void main(String[] args){
+           String[] arrays = {"Lei" , "Marrion" , "Alden" , "Marc" , "Vezayne"};
+
+             for(String loop : arrays){
+                 System.out.print(loop + " , ");
+             }
+     }
+}

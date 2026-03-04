2b264e2e33320f18ddb3c3f169c8b4c664a9d3b7
Lei Justine <clemente.leijustine@gmail.com>
2026-03-04 19:52:06 +0800

    Adding while loop

---
+++ src/Basic_Fundamentals/Factorial.java
@@ -0,0 +1,19 @@
+package Basic_Fundamentals;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        int sum = 1;
+        for(int i = n; i > 0; i--){
+            sum*=i;
+        }
+
+        System.out.println("Total: " + sum);
+    }
+}

950297dae17da5056f21f96c6c6b8a987ae6b1f4
Lei Justine <clemente.leijustine@gmail.com>
2026-03-04 19:42:07 +0800

    adding Input-Output

---
+++ src/Basic_Fundamentals/InputOutput.java
@@ -0,0 +1,18 @@
+package Basic_Fundamentals;
+
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Name: " + name + "\n" + "Age: " + age);
+
+    }
+}

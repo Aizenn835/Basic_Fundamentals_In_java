4937dd98ba1a400cc77ea4ca7ad7134fe8bfa288
Lei Justine Clemente <clemente.leijustine@gmail.com>
2026-03-16 20:08:25 +0800

    Add main method to DataTypes class with example data

--- src/Arrays_and_Strings/DataTypes.java
+++ src/Arrays_and_Strings/DataTypes.java
@@ -1,4 +1,18 @@
 package Arrays_and_Strings;
 
 public class DataTypes {
+    public static void main(String[] args) {
+
+        String name = "Lei Justine Clemente";
+        int age = 19;
+        float grade = 1.25f;
+        double weight = 52.6;
+        boolean college = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Weight: " + weight);
+        System.out.println("College: " + college);
+    }
 }
